ca2f702cef40d01b9efbb635f17ba95646d05e42
Andrew-Mtz <andrew.nahuelMH@gmail.com>
2021-09-01 00:20:30 -0300

    all routes and pages added

--- src/routes/NavRouter.jsx
+++ src/routes/NavRouter.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import NavBar from '../components/NavBar.jsx';
 
 const NavRouter = ({ path, children }) => (
   <Route path={path}>
-    <NavBar />
     {children}
   </Route>
 );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from '../App';
-import NavBar from '../components/NavBar.jsx';
-import Inicio from '../pages/Inicio';
+import NavBar from '../components/NavBar';
+import {
+  Inicio, LogIn, SignUp, Home,
+} from '../pages/Pages';
 import NavRouter from './NavRouter';
 
 const Routes = () => (
   <Switch>
     <NavRouter path="/inicio">
       <Inicio />
+    </NavRouter>
+    <NavRouter path="/login">
+      <LogIn />
+    </NavRouter>
+    <NavRouter path="/signup">
+      <SignUp />
+    </NavRouter>
+    <NavRouter path="/home">
+      <Home />
     </NavRouter>
     <Route path="/">
       <NavBar />
